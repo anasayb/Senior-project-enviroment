1fd6f70b97e290e61470cee18dc0aec59d83d27a
anasayb <anasayb@users.noreply.github.com>
2022-11-30 22:02:35 +0300

    Controlled speed of cars, cars stopes before colliding, traffic light controllers

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.AssetImporters;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
 
     public WheelCollider[] wheels;
-    public float MAX_SPEED = 200f;
-    
-    
+    public float MAX_SPEED = 40f;
 
+    [Header("Sensors")]
+    public float sensorLength = 25f;
+    private bool colide = false;
 
     // Start is called before the first frame update
     void Start()
@@ -25,11 +28,48 @@ public class CarController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        
-        wheels[0].motorTorque = MAX_SPEED;
-        wheels[1].motorTorque = MAX_SPEED;
-        wheels[2].motorTorque = MAX_SPEED;
-        wheels[3].motorTorque = MAX_SPEED;
+
+        checkColide();
+
+        if (!colide) {
+            
+            Rigidbody speed = GetComponent<Rigidbody>();
+            if (speed.velocity.magnitude > MAX_SPEED)
+            {
+                return;
+            }
+            for (int i = 0; i < wheels.Length; i++)
+            {
+                wheels[i].motorTorque = MAX_SPEED;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < wheels.Length; i++)
+            {
+                wheels[i].brakeTorque = 500;
+            }
+        }
+
+    }
+
+    private void checkColide()
+    {
+
+        RaycastHit hit;
+        Vector3 pos = transform.position;
+        Vector3 test = new Vector3(0f, 0f, 1f);
+        if (Physics.Raycast(pos, test, out hit, sensorLength))
+        {
+            print("hello its me the freaking "+transform.gameObject.name);
+            colide = true;
+            return;
+        }
+
+        //Debug Code
+        //Debug.DrawRay(transform.position, test*10, Color.red);
+
+        colide = false;
 
     }
 }
